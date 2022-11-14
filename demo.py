4b0d952ad049197491fc1788f3e81ff7de5cb2a6
aidan-explore <aidan@explore-ai.net>
2022-11-14 16:28:19 +0200

    groupby role, mission, client

--- demo.py
+++ demo.py
@@ -8,6 +8,19 @@ import logging
 AIRTABLE_TOKEN = os.environ['AIRTABLE_TOKEN']
 AIRTABLE_BASE_ID = "appTZVq2CZuxr4CoZ"
 
+# REFACTOR: not needed as the data shouold not allow for many-to-many relationships
+def get_first_list(input):
+    if type(input) == str:
+        return input
+    elif type(input) == list:
+        return input[0]
+    
+    return None
+
+def get_relation_name(airtable : Table, field: str) -> dict:
+    get_data = {k['id']: k['fields'][field] for k in airtable.all()}
+    return get_data
+
 def create_relations(input_series : pd.Series) -> dict:
     dict_relation = {}
 
@@ -17,27 +30,25 @@ def create_relations(input_series : pd.Series) -> dict:
         
         for relation in relation_list:
             if relation in dict_relation.keys():
-                dict_relation[relation]['links'].append(_id)
+                dict_relation[relation].append(_id)
             else:
-                dict_relation[relation] = {'links' : [_id]}
+                dict_relation[relation] = [_id]
                 
     return dict_relation
 
-def get_relation_name(dict_relation : dict, airtable : Table, field: str) -> dict:
-    get_data = {k['id']: k['fields'][field] for k in airtable.all()}
-    for k in dict_relation.keys():
-        dict_relation[k]['name'] = get_data[k]
+@st.experimental_memo()
+def get_relations(df, table_name:str = "Roles", column_name:str = "Role"):
     
-    return dict_relation
-
-# REFACTOR: not needed as the data shouold not allow for many-to-many relationships
-def get_first_list(input):
-    if type(input) == str:
-        return input
-    elif type(input) == list:
-        return input[0]
+    air_table      = Table(AIRTABLE_TOKEN, AIRTABLE_BASE_ID, table_name)
+    dict_lookups   = get_relation_name(air_table, column_name)
+    df_lookups     = pd.DataFrame.from_dict(dict_lookups, orient="index", columns=[column_name + "_Name"])
+    dict_relations = create_relations(df[column_name])
     
-    return None
+    df['_' + column_name] = df[column_name].apply(get_first_list)
+    
+    df = df.join(df_lookups, on='_' + column_name)
+    
+    return df, dict_lookups, dict_relations
 
 @st.experimental_memo()
 def get_requirements():
@@ -75,19 +86,6 @@ def summarise_start_end(df: pd.DataFrame, freq = "1M", start_col: str = '_start_
     
     return df_temp
 
-@st.experimental_memo()
-def get_relations(df, column_name:str = "Role"):
-    
-    roles       = Table(AIRTABLE_TOKEN, AIRTABLE_BASE_ID, "Roles")
-    dict_roles  = create_relations(df[column_name])
-
-    get_relation_name(dict_roles, roles, column_name)
-    
-    df[column_name + "_lookup"] = df[column_name].apply(lambda x: dict_roles[x[0]]['name'])
-    
-    return df, dict_roles
-
-
 # TODO: fix get_state error in caching this request
 # @st.experimental_memo(ttl=60 * 60 * 24)
 def get_chart(data, start_date: dt.datetime, end_date: dt.datetime, 
@@ -107,7 +105,7 @@ def get_chart(data, start_date: dt.datetime, end_date: dt.datetime,
         
     st.bar_chart(df_chart)
     
-    st.dataframe(df)
+    st.dataframe(df[['Client_Name', "Mission_Name", "Role_Name", "Capacity"]].drop_duplicates())
 
 st.set_page_config(
     page_title="ExploreAI Mission Requirements", page_icon="⬇", layout="centered"
@@ -117,7 +115,9 @@ st.image(image='eai.png',caption='https://explore.ai')
 
 df = get_requirements()
 df = summarise_start_end(df)
-df, role_dict = get_relations(df)
+df, roles, role_dict       = get_relations(df, "Roles", "Role")
+df, missions, mission_dict = get_relations(df, "Mission", "Mission")
+df, clients, client_dict   = get_relations(df, "Clients", "Client")
 
 st.title("⬇ Mission Requirements")
 st.write("Number of people required by month")
@@ -125,8 +125,10 @@ st.write("Number of people required by month")
 # define the streamlit sidebar
 choice_start   = st.sidebar.date_input('Select your start date:', value=pd.to_datetime('2022-04-01')) 
 choice_end     = st.sidebar.date_input('Select your end date:', value=pd.to_datetime('2023-03-31')) 
-choice_groupby = st.sidebar.radio('Select what to group by:', ('None', 'Seniority', "Role_lookup")) 
-choice_role    = st.sidebar.multiselect('Select your roles:', set([v['name'] for v in role_dict.values()]))
+choice_groupby = st.sidebar.radio('Select what to group by:', ('None', 'Seniority', "Role_Name", "Mission_Name", 'Client_Name')) 
+choice_role    = st.sidebar.multiselect('Select your roles:', set([v for v in roles.values()]))
+choice_client  = st.sidebar.multiselect('Select your Client:', set([v for v in clients.values()]), disabled=True)
+choice_mission = st.sidebar.multiselect('Select your mission:', set([v for v in missions.values()]), disabled=True)
     
 chart = get_chart(df, start_date=choice_start, end_date=choice_end, roles=choice_role, groupby=choice_groupby)
 
