a94cc5edd71db16fef826164076baa6208db132f
aidan-explore <aidan@explore-ai.net>
2022-11-22 13:30:48 +0200

    added in projected requirements

--- demo.py
+++ demo.py
@@ -1,26 +1,32 @@
-import os
-import pandas as pd
 import datetime as dt
+import os
+
+import pandas as pd
+
 import streamlit as st
+import altair as alt
+
 from pyairtable import Table
-import logging
 
 AIRTABLE_TOKEN = os.environ['AIRTABLE_TOKEN']
 AIRTABLE_BASE_ID = "appTZVq2CZuxr4CoZ"
 
 AIRTABLE_SCHEMA = {
-    'Mission Requirements' : ['Requirement', 'Mission', 'Role', 'Seniority', 'Capacity', 'Client', '_start_date', '_end_date', '_prob', "Scenario"],
-    'Mission Logs' : ['Client', 'Mission Log', 'Mission', 'Role', 'Seniority', 'EXPLORER', 'Capacity', '_start_date', '_end_date', 'mission_requirement', "Scenario"]
+    'Mission Requirements' : ['Requirement', 'Mission', 'Role', 'Seniority', 'Capacity', 'Client', "_renewal", '_start_date', '_end_date', '_prob', "Scenario"],
+    'Mission Logs' : ['Client', 'Mission Log', 'Mission', 'Role', 'Seniority', 'EXPLORER', 'Capacity', "_prob", "_renewal", '_start_date', '_end_date', 'mission_requirement', "Scenario"]
 }
 
-# REFACTOR: not needed as the data shouold not allow for many-to-many relationships
+def get_opportunities():
+    return "https://main-apiservice.azurewebsites.net/opportunities/all?format=csv"
+
 def get_first_list(input):
     if type(input) == str:
         return input
+    
     elif type(input) == list:
         return input[0]
     
-    return None
+    return input
 
 @st.experimental_memo()
 def get_relation_name(table_name: str, field: str) -> pd.DataFrame:
@@ -72,7 +78,7 @@ def get_requirements(table_name:str) -> pd.DataFrame:
 
     return df
 
-@st.experimental_memo()
+# @st.experimental_memo()
 def summarise_start_end(df: pd.DataFrame, freq = "1M", start_col: str = '_start_date', end_col: str = '_end_date', format='%Y-%m-%d') -> pd.DataFrame:
     
     df = df.reset_index()
@@ -88,41 +94,51 @@ def summarise_start_end(df: pd.DataFrame, freq = "1M", start_col: str = '_start_
     df_dates = pd.DataFrame([(i, end.replace(day=1), end) for i, end in enumerate(pd.date_range(start=min_date, end=max_date, freq=freq))], columns=['index', 'Month_Start', 'Month_End'])
     df_dates.set_index('index', inplace=True)
 
-    df_temp = df.merge(df_dates, how='cross')
+    df_temp = df.merge(df_dates, how='cross', indicator=True)
     
-    df_temp = df_temp[df_temp['_start'] <= df_temp['Month_Start']]
-    df_temp = df_temp[df_temp['_end'] >= df_temp['Month_End']]
+    df_temp['_capacity'] = df_temp['Capacity'].astype('float')
+    df_temp.loc[df_temp['_start'] > df_temp['Month_Start'], '_capacity'] = 0.0
+    df_temp.loc[df_temp['_end'] < df_temp['Month_End'], '_capacity'] = 0.0
     
+    df_temp['_projected'] = df_temp['_capacity'] * df_temp['_probability']
+    df_temp.loc[df_temp["Month_End"] > df_temp['_end'], '_projected'] = df_temp["Capacity"] * df_temp['_renewal']
+        
     return df_temp
 
-@st.experimental_memo()
-def get_chart(data, start_date: dt.datetime, end_date: dt.datetime, 
-              roles: list, clients: list, missions:list,
-              groupby: str, values: str = "Capacity"):
+def df_filter_dates(data: pd.DataFrame, start_date: dt.datetime, end_date: dt.datetime) -> pd.DataFrame:
+    
     mask = (data['Month_End'] >= pd.to_datetime(start_date)) & (data['Month_End'] <= pd.to_datetime(end_date))
     df = data[mask]
     
-    if roles:    
-        mask = df["Role_Name"].isin(roles)
-        df = df[mask]
-        
-    if missions:
-        mask = df['Mission_Name'].isin(missions)
-        df = df[mask]
-        
-    if clients:
-        mask = df['Client_Name'].isin(clients)
-        df = df[mask]
-    
-    if groupby in df.columns:
-        df_chart = df.pivot_table(columns=groupby, index='Month_End', values=values, aggfunc="sum")
-        
-    else:
-        df_chart = df.groupby(["Month_End"]).sum(values)
-        
-    st.bar_chart(df_chart)
-    
     return df
+    
+
+def df_filter_isin(data: pd.DataFrame, mask_dict:dict) -> pd.DataFrame:
+    
+    df = data
+    
+    for col_name, look_for in mask_dict.items():
+        if col_name in df.columns:
+            if look_for:
+                mask = df[col_name].isin(look_for)
+                df = df[mask]
+                
+    return df
+
+
+def get_base_chart(data, groupby: str, values: str = "_capacity"):
+    
+    base = alt.Chart(data).encode(x="Month_End")
+    bar = base.mark_bar(size=20).encode(y=f"sum({values})", color=f"{groupby}:O")
+    
+    return base, bar
+
+def get_projected_chart(base: alt.Chart):
+    
+    line = base.mark_line(color='red').encode(y="sum(_prob_require)")
+    line_projected = base.mark_line(color='red', strokeDash=[1, 5], ).encode(y="sum(_projected)")
+    
+    return line, line_projected
 
 st.set_page_config(
     page_title="ExploreAI Mission Requirements", page_icon="⬇", layout="centered"
@@ -136,10 +152,27 @@ clients   = get_relation_name("Clients", "Client")
 explorers = get_relation_name("EXPLORER", "EXPLORER")
 scenarios = get_relation_name("Scenarios", "Scenario")
 
+# define the streamlit sidebar
+choice_start    = st.sidebar.date_input('Select your start date:', value=pd.to_datetime('2022-04-01')) 
+choice_end      = st.sidebar.date_input('Select your end date:', value=pd.to_datetime('2023-06-30')) 
+choice_groupby  = st.sidebar.radio('Select what to group by:', ('None', 'Seniority', "Role_Name", "Mission_Name", 'Client_Name', "Scenario")) 
+choice_role     = st.sidebar.multiselect('Select your Roles:', roles['Role_Name'].unique())
+choice_client   = st.sidebar.multiselect('Select your Client:', clients['Client_Name'].unique())
+choice_mission  = st.sidebar.multiselect('Select your Mission:', missions['Mission_Name'].unique())
+choice_scenario = st.sidebar.multiselect('Select your Scenarios:', scenarios['Scenario_Name'].unique(), disabled=True)
+
+requirements_filter = {
+    "Role_Name" : choice_role,
+    "Mission_Name" : choice_mission,
+    "Client_Name" : choice_client,
+    "Scenario_Name" : choice_scenario,
+    }
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Requirements", "Allocations", "Gaps", "Explorers", "All Data"])
+
 @st.experimental_memo(ttl=60 * 60 * 24)
 def get_log_table(table_name:str):
     df = get_requirements(table_name)    
-    df = summarise_start_end(df)
     
     link_dict = {}
     df, link_dict['Role']      = get_relations(df, roles, "Role")
@@ -150,31 +183,45 @@ def get_log_table(table_name:str):
     
     return df, link_dict
 
-@st.experimental_memo()
+# @st.experimental_memo()
 def get_mission_requirements():
-
+    
     df, _  = get_log_table('Mission Requirements')
     df     = df[df['Scenario'] != "9. Lost - 0%"]
+    df['_probability'] = df['_prob'].apply(get_first_list)
+    df['_probability'].fillna(0.0, inplace=True)
+    df['_probability'] = df['_probability'].astype(float)
+        
+    df = summarise_start_end(df) 
     
+    df['_prob_require'] = df['_capacity'] * df['_probability']
+       
     return df
 
-@st.experimental_memo()
+# @st.experimental_memo()
 def get_mission_logs():
     
     df, _  = get_log_table('Mission Logs')
     df['_requirement'] = df['mission_requirement'].apply(get_first_list)
     df = df[df["Scenario"] != "Rejected"]
     
-    df_require = df.groupby(['_requirement', 'Month_End']).sum('Capacity')
-    df_require.rename(columns={'Capacity' : 'Allocation'}, inplace=True)
+    df['_probability'] = df['_prob'].apply(get_first_list)
+    df['_probability'].fillna(0.0, inplace=True)
+    df['_probability'] = df['_probability'].astype(float)
+    
+    df = summarise_start_end(df) 
+    
+    df_require = df.groupby(['_requirement', 'Month_End']).sum('_capacity')
+    df_require.rename(columns={'_capacity' : 'Allocation'}, inplace=True)
     
     return df, df_require
 
 @st.experimental_memo()
-def get_require_gaps(df_requirements, df_require_logs):
-    df_gaps = df_requirements.join(df_require_logs, on=['index', 'Month_End'])
+def get_require_gaps(df_requirements: pd.DataFrame, df_require_logs: pd.DataFrame) -> pd.DataFrame:
+    
+    df_gaps = df_requirements.join(df_require_logs, on=['index', 'Month_End'], rsuffix="logs")
     df_gaps['Allocation'].fillna(0, inplace=True)
-    df_gaps['Gap'] = df_gaps['Capacity'] - df_gaps['Allocation']
+    df_gaps['Gap'] = df_gaps['_capacity'] - df_gaps['Allocation']
     
     return df_gaps
 
@@ -189,43 +236,59 @@ def get_explorer_allocations(df_mission_logs):
     df_explorers['Month_End'] = df_explorers['Month_End'].dt.date
     
     return df_explorers
-    
+
 df_requirements = get_mission_requirements()
 df_mission_logs, df_require_logs = get_mission_logs()
 df_gaps = get_require_gaps(df_requirements, df_require_logs)
 df_explorers = get_explorer_allocations(df_mission_logs)
-
-# define the streamlit sidebar
-choice_start    = st.sidebar.date_input('Select your start date:', value=pd.to_datetime('2022-04-01')) 
-choice_end      = st.sidebar.date_input('Select your end date:', value=pd.to_datetime('2023-03-31')) 
-choice_groupby  = st.sidebar.radio('Select what to group by:', ('None', 'Seniority', "Role_Name", "Mission_Name", 'Client_Name', "Scenario")) 
-choice_role     = st.sidebar.multiselect('Select your Roles:', roles['Role_Name'].unique())
-choice_client   = st.sidebar.multiselect('Select your Client:', clients['Client_Name'].unique())
-choice_mission  = st.sidebar.multiselect('Select your Mission:', missions['Mission_Name'].unique())
-choice_scenario = st.sidebar.multiselect('Select your Scenarios:', scenarios['Scenario_Name'].unique(), disabled=True)
-
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Requirements", "Allocations", "Gaps", "Explorers", "All Data"])
     
+with tab5:
+    st.dataframe(scenarios)
+
+    st.write("Requirements table")
+    st.dataframe(df_requirements)
+    
+    st.write("Mission Logs")
+    st.dataframe(df_mission_logs)
+    
+    st.write("add up requirements")
+    st.dataframe(df_gaps)
+
 with tab1:
     st.title("Mission Requirements")
     st.write("Number of people required by month")
     
-    df = get_chart(df_requirements, start_date=choice_start, end_date=choice_end, 
-                   roles=choice_role, clients=choice_client, missions=choice_mission, groupby=choice_groupby)
+    df = df_filter_dates(df_requirements, start_date=choice_start, end_date=choice_end)
+    df = df_filter_isin(df, requirements_filter)
+    
+    base, bar = get_base_chart(df, groupby=choice_groupby)
+    line, projected = get_projected_chart(base)
+    
+    st.altair_chart((bar+line+projected), use_container_width=True)
+    
     st.dataframe(df[['Client_Name', "Mission_Name", "Role_Name", 'Seniority', "Capacity"]].drop_duplicates())
     
 with tab2:
     st.title("Mission Logs")
     st.write("Number of people allocated by month")
-    df = get_chart(df_mission_logs, start_date=choice_start, end_date=choice_end, 
-                   roles=choice_role, clients=choice_client, missions=choice_mission, groupby=choice_groupby)
+    
+    df = df_filter_dates(df_mission_logs, start_date=choice_start, end_date=choice_end)
+    df = df_filter_isin(df, requirements_filter)
+    
+    base, bar = get_base_chart(df, groupby=choice_groupby)
+    st.altair_chart(bar, use_container_width=True)
+    
     st.dataframe(df[["Mission_Name", "Role_Name", 'EXPLORER_Name', "Capacity"]].drop_duplicates())
 
 with tab3:
     st.title("Gaps in allocation")
-    df = get_chart(df_gaps, start_date=choice_start, end_date=choice_end, 
-                   roles=choice_role, clients=choice_client, missions=choice_mission, groupby=choice_groupby, values="Gap")
     
+    df = df_filter_dates(df_gaps, start_date=choice_start, end_date=choice_end)
+    df = df_filter_isin(df, requirements_filter)
+    
+    base, bar = get_base_chart(df, groupby=choice_groupby, values="Gap")    
+    st.altair_chart(bar, use_container_width=True)
+        
     df = df[df['Gap'] != 0]
     st.dataframe(df[["Mission_Name", "Role_Name", 'Seniority', 'Month_End', "Capacity", 'Allocation', 'Gap']].drop_duplicates())
     
@@ -246,17 +309,6 @@ with tab4:
 
     st.dataframe(df.drop_duplicates())
 
-    
-with tab5:
-    st.dataframe(scenarios)
 
-    st.write("Requirements table")
-    st.dataframe(df_requirements)
-    
-    st.write("Mission Logs")
-    st.dataframe(df_mission_logs)
-    
-    st.write("add up requirements")
-    st.dataframe(df_gaps)
     
 
